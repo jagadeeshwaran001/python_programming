15374e286d8cae4afe980f9f0ab51187ea1a1ffb
jagadeeshwaran001 <45691973+jagadeeshwaran001@users.noreply.github.com>
2020-02-20 22:19:41 +0530

    Add files via upload

---
+++ codechef_school_of_geomentry.py
@@ -0,0 +1,18 @@
+try:
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        
+        a=list(map(int, input().split()))
+        b=list(map(int, input().split()))
+        
+        a=sorted(a)
+        b=sorted(b)
+        ans=0
+        for j in range(n):
+            if(a[j]!=b[j]):
+                ans=ans+(min(a[j],b[j]))
+        print(ans)
+except:
+    pass
+    
