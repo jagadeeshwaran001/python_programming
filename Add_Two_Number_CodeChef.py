88a1289eab9bcf11e89fb4d6f6295fc35bd1383d
jagadeeshwaran001 <45691973+jagadeeshwaran001@users.noreply.github.com>
2020-03-04 21:14:50 +0530

    Add files via upload

---
+++ Add_Two_Number_CodeChef.py
@@ -0,0 +1,8 @@
+#link for the problem:-https://www.codechef.com/problems/FLOW001
+T = int(input())
+for tc in range(T):
+	# Read integers a and b.
+	a, b = map(int, input().split(' '))
+	
+	ans = a + b
+	print(ans)
